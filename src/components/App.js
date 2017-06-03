014538441ff120c3671bd23d40862589ebc4776e
Jim Parker <jim@jiparker.com>
2017-06-03 05:46:06 -0500

    0001 - remove additional demo stuff, fixed tests - JP

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, IndexLink } from 'react-router';
+import {Link, IndexLink} from 'react-router';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
@@ -11,11 +11,8 @@ class App extends React.Component {
       <div>
         <IndexLink to="/">Home</IndexLink>
         {' | '}
-        <Link to="/fuel-savings">Demo App</Link>
-        {' | '}
         <Link to="/about">About</Link>
-        <br/>
-        {this.props.children}
+        <br/> {this.props.children}
       </div>
     );
   }
